46b6bbcba4fd816cc372ba0285e2ecc0dcc2f3a5
Koen Sengers <k.sengers@gynzy.com>
2017-09-01 15:15:57 +0200

    micropost tests afgerond

--- spec/controllers/microposts_controller_spec.rb
+++ spec/controllers/microposts_controller_spec.rb
@@ -26,14 +26,14 @@ describe MicropostsController do
 		end
 
 		it 'When user is found with micropost' do
-			micropost = Micropost.create(content: "Test", user_id: user.id)
+			micropost = Micropost.create(content: "Winter is coming", user_id: user.id)
 
 			get :index, format: :json, user_id: user.id
 			parse_body = JSON.parse(response.body)
 			returned_micropost = parse_body.first
 
 			expect(returned_micropost["id"]).to eq micropost.id
-			expect(returned_micropost["content"]).to eq "Test"
+			expect(returned_micropost["content"]).to eq "Winter is coming"
 			expect(returned_micropost["user_id"]).to eq user.id
 
 		end
@@ -46,22 +46,91 @@ describe MicropostsController do
 			sign_in(user, no_capybara: true)
 			post :create, micropost: {content: "Winter is coming"}
 
-			microposts = Micropost.where(content: "Winter is coming")
-			micropost = microposts.first
+			expect(assigns(:micropost)).to be_persisted
+			expect(assigns(:micropost).user_id).to eq user.id
+			expect(response).to redirect_to(root_url)
+			expect(flash.to_hash[:success]).to eq "Micropost created!"
 
-			expect(microposts).not_to be_empty
-			expect(micropost.user_id).to eq user.id
 
 		end
 
-		# it "Micropost empty" do
-		# 	sign_in(user)
-		# 	post :create, micropost: {content: ""}
+		it  "Micropost created fail" do
+			sign_in(user, no_capybara: true)
+			post :create, micropost: {content: "a" * 200}
 
-		# 	expect(page).to have_content("Content can't be blank")
+			expect(assigns(:micropost)).to_not be_persisted
+			expect(response).to render_template('static_pages/home')
 
-		# end
+		end
 
+	end
+
+	describe "POST JSON destroy" do
+
+		it "should delete a micropost" do
+			sign_in(user, no_capybara: true)
+			micropost = Micropost.create(content: "Winter is coming", user_id: user.id)
+
+			delete :destroy, id: micropost.id
+
+			expect(assigns(:micropost)).to_not be_persisted
+			expect(response).to redirect_to(root_url)			
+
+		end
+
+		it "should not delete a micropost from another user" do
+			sign_in(user, no_capybara: true)
+			micropost = Micropost.create(content: "Winter is coming", user_id: user.id + 1)
+
+			delete :destroy, id: micropost.id
+
+			expect(micropost).to be_persisted
+			expect(response).to redirect_to(root_url)
+
+		end
 
 	end	
+
+	describe "POST JSON update" do
+
+		it "should update a micropost" do
+			sign_in(user, no_capybara: true)
+			micropost = Micropost.create(content: "Winter is coming", user_id: user.id)
+
+			patch :update, id: micropost.id, micropost: { :content => "Winter has come" }
+			micropost.reload
+
+			expect(micropost).to be_persisted
+			expect(micropost["content"]).to eq "Winter has come"
+			expect(flash.to_hash[:success]).to eq "Post updated"
+			expect(response).to redirect_to(root_url)
+			expect(micropost["user_id"]).to eq user.id
+
+		end
+
+		it "should not update a micropost from another user" do
+			sign_in(user, no_capybara: true)
+			micropost = Micropost.create(content: "Winter is coming", user_id: user.id + 1)
+
+			patch :update, id: micropost.id, micropost: { :content => "Winter has come" }
+			micropost.reload
+
+			expect(micropost["content"]).to eq "Winter is coming"
+			expect(response).to redirect_to(root_url)
+
+		end
+
+		it "should not update a micropost with empty content" do
+			sign_in(user, no_capybara: true)
+			micropost = Micropost.create(content: "Winter is coming", user_id: user.id)
+
+			patch :update, id: micropost.id, micropost: { :content => "" }
+			micropost.reload
+
+			expect(response).to render_template('edit')
+			expect(micropost['content']).to eq("Winter is coming")
+			
+		end
+	end
+
 end
